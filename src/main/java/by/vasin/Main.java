66ffca80420ff61c04ee05ac5c2d77508db2618a
Vasiliyev Ilia <it.ilya97@gmail.com>
2023-03-20 16:55:43 +0300

    Отрефакторил. Убрал ненужные импорты

--- src/main/java/by/vasin/Main.java
+++ src/main/java/by/vasin/Main.java
@@ -1,10 +1,8 @@
 package by.vasin;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Main {
 
